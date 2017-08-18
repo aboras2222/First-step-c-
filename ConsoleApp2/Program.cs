15c938af636a47af4f9aad44286ad49a4c203d88
Eng.Abouras <Eng.Abouras@DESKTOP-GUDPHEL>
2017-08-18 06:14:29 -0700

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//The First Program with c# ...
+//Was Complicated...But it's DONN.
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+
+
+          Console.Write("Enter the number that you want to close it :");
+
+            string s=Console.ReadLine();
+            int y ;
+
+            double d;
+            d = Convert.ToDouble (s) ; 
+
+            y = Convert.ToInt32(d);
+        
+            Console.Write("The Number is :");
+            Console.WriteLine(y);
+            Console.WriteLine("Thank you from Eng Abouras");
+           
+   
+
+
+            
+            
+
+            
+            
+   
+           
+       
+ 
+            
+            
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+
+
+
+      
+        }
+    }
+}
